06ac9e58602b2ed102c36d04bc5b92309bafcd19
tmkn <tuomasklein@gmail.com>
2019-08-17 17:52:09 +0200

    refactored mock server

--- tests/test1.test.ts
+++ tests/test1.test.ts
@@ -1,5 +1,4 @@
 import * as path from "path";
-import * as fs from "fs";
 import { Server } from "http";
 import * as express from "express";
 import * as puppeteer from "puppeteer";
@@ -7,11 +6,13 @@ import test from "ava";
 
 import { evaluate2 } from "../src/evaluate2";
 
-test("Basic Test", async t => {
-    const server = new MockServer();
+test(`Basic Test`, async t => {
+    const port = 3000;
+    const server = await MockServer.Create(port);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
+    
+    await page.goto(`http://localhost:${port}`);
 
     let innerText = await evaluate2<string>(page, _path(`./tests/code.js`));
     t.is(innerText, `hello world`);
@@ -20,27 +21,44 @@ test("Basic Test", async t => {
     server.close();
 });
 
+test.todo(`Complex Test`);
+
 function _path(relativePath: string): string {
     return path.join(__dirname, `../..`, relativePath);
 }
 
 class MockServer {
     private _server: Server;
-    private readonly _port = 3000;
 
-    constructor() {
+    private constructor(private readonly _port: number, cb?: () => void) {
         const app = express();
 
         app.get(`/`, (req, res) => {
             res.sendFile(_path(`./tests/index.html`));
         });
 
-        this._server = app.listen(this._port, () => console.log(`Started MockServer`));
+        this._server = app.listen(this._port, () => {
+            console.log(`Started MockServer`);
+
+            if (cb) {
+                cb();
+            }
+        });
     }
 
     close(): void {
         this._server.close(e => {
             if (e) console.log(e);
+        });
+    }
+
+    static Create(port: number): Promise<MockServer> {
+        return new Promise((resolve, reject) => {
+            try {
+                const server: MockServer = new MockServer(port, () => resolve(server));
+            } catch (e) {
+                reject(e);
+            }
         });
     }
 }
